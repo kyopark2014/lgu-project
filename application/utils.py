507d7d3f71302b930a278a03c955418494dab36e
Kyoungsu Park <52392004+kyopark2014@users.noreply.github.com>
2025-09-19 19:47:27 +0900

    Update utils.py

--- application/utils.py
+++ application/utils.py
@@ -3,6 +3,7 @@ import sys
 import json
 import boto3
 import os
+import time
 
 from langchain_community.utilities.tavily_search import TavilySearchAPIWrapper
 
@@ -53,10 +54,207 @@ if not accountId:
     with open(config_path, "w", encoding="utf-8") as f:
         json.dump(config, f, indent=2)
 
-bedrock_region = config.get('region', 'us-west-2')
+bedrock_region = config.get("region", "")
 logger.info(f"bedrock_region: {bedrock_region}")
-projectName = config.get('projectName', 'lgu')
+projectName = config.get("projectName", "")
 logger.info(f"projectName: {projectName}")
+
+# Bucket for Knowledge Base
+bucket_name = config.get("bucket_name", "")
+logger.info(f"bucket_name: {bucket_name}")
+
+if not bucket_name:
+    bucket_name = f"storage-for-{projectName}-{accountId}-{bedrock_region}"
+    config['bucket_name'] = bucket_name
+    
+    s3 = boto3.client('s3', region_name=bedrock_region)
+    response = s3.list_buckets()
+    buckets = response.get('Buckets', [])
+    if not any(bucket['Name'] == bucket_name for bucket in buckets):
+        logger.info(f"bucket_name: {bucket_name} is not exists.")
+        
+        s3.create_bucket(
+            Bucket=bucket_name,
+            CreateBucketConfiguration={'LocationConstraint': bedrock_region}
+        )
+        logger.info(f"bucket_name: {bucket_name} is created.")    
+        
+        s3.put_object(Bucket=bucket_name, Key='docs/')
+        logger.info(f"docs folder is created in {bucket_name}.")
+        
+    with open(config_path, "w", encoding="utf-8") as f:
+        json.dump(config, f, indent=2)
+
+# Knowledge Base
+knowledge_base_id = config.get('knowledge_base_id')
+logger.info(f"knowledge_base_id: {knowledge_base_id}")    
+
+if not knowledge_base_id:
+    logger.info(f"knowledge_base_id is required.")
+    
+    knowledge_base_name = projectName
+    config['knowledge_base_name'] = knowledge_base_name
+    
+    role_name = f"role-knowledge-base-for-{projectName}-{bedrock_region}"
+    iam = boto3.client('iam', region_name=bedrock_region)
+    response = iam.list_roles(MaxItems=200)
+    # logger.info(f"response of list_roles: {response}")
+    
+    roles = response.get('Roles', [])
+    if not any(role['RoleName'] == role_name for role in roles):
+        logger.info(f"role_name: {role_name} is not exists.")
+        response = iam.create_role(RoleName=role_name, AssumeRolePolicyDocument=json.dumps({
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Principal": {"Service": "bedrock.amazonaws.com"},
+                    "Action": "sts:AssumeRole"
+                }
+            ]
+        }))
+        role_arn = response['Role']['Arn']
+        config['knowledge_base_role_arn'] = role_arn
+        
+        iam.put_role_policy(RoleName=role_name, PolicyName=f"knowledge-base-for-{projectName}-{bedrock_region}", PolicyDocument=json.dumps({
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Sid": "BedrockAllStatement",
+                    "Action": "bedrock:*",
+                    "Resource": "*",
+                    "Effect": "Allow"
+                },
+                {
+                    "Sid": "BedrockInvokeInferenceProfileStatement",
+                    "Effect": "Allow",
+                    "Action": [
+                        "bedrock:GetInferenceProfile",
+                        "bedrock:InvokeModel"
+                    ],
+                    "Resource": "*"
+                },
+                {
+                    "Sid": "S3BucketStatement",
+                    "Effect": "Allow",
+                    "Action": ["s3:*"],
+                    "Resource": "*"
+                }
+            ]
+        }))
+        
+        logger.info(f"role_name: {role_name} is created.")
+    
+    client = boto3.client('s3vectors')
+    
+    s3_vector_bucket_name = config.get('s3_vector_bucket_name', "")
+    s3_vector_bucket_arn = config.get('s3_vector_bucket_arn', "")
+    if not s3_vector_bucket_name or not s3_vector_bucket_arn:
+        s3_vector_bucket_name = f"s3-vector-for-{projectName}-{accountId}-{bedrock_region}"
+        logger.info(f"s3_vector_bucket_name: {s3_vector_bucket_name}")
+        config['s3_vector_bucket_name'] = s3_vector_bucket_name
+                
+        response = client.list_vector_buckets(maxResults=50)
+        logger.info(f"response of list_vector_buckets: {response}")
+        
+        vectorBuckets = response.get('vectorBuckets', [])
+        if any(vectorBucket['vectorBucketName'] == s3_vector_bucket_name for vectorBucket in vectorBuckets):
+            for vectorBucket in vectorBuckets:
+                if vectorBucket['vectorBucketName'] == s3_vector_bucket_name:
+                    s3_vector_bucket_arn = vectorBucket['vectorBucketArn']
+                    logger.info(f"s3_vector_bucket_arn: {s3_vector_bucket_arn}")
+                    break
+        else:
+            response = client.create_vector_bucket(vectorBucketName=s3_vector_bucket_name)
+            logger.info(f"response of create_vector_bucket: {response}")
+            logger.info(f"s3_vector_bucket_name: {s3_vector_bucket_name} is created.")
+            s3_vector_bucket_arn = response['vectorBucketArn']
+            logger.info(f"s3_vector_bucket_arn: {s3_vector_bucket_arn}")
+            
+        config['s3_vector_bucket_arn'] = s3_vector_bucket_arn
+        with open(config_path, "w", encoding="utf-8") as f:
+            json.dump(config, f, indent=2)
+            
+    s3_vector_index_name = config.get('s3_vector_index_name', "")    
+    s3_vector_index_arn = config.get('s3_vector_index_arn', "")
+    
+    if not s3_vector_index_name:
+        s3_vector_index_name = f"s3-vector-index-for-{projectName}-{accountId}-{bedrock_region}"
+        logger.info(f"s3_vector_index_name: {s3_vector_index_name}")
+        config['s3_vector_index_name'] = s3_vector_index_name
+        
+        response = client.list_indexes(vectorBucketName=s3_vector_bucket_name)
+        indexes = response.get('indexes', [])
+        
+        if any(index['indexName'] == s3_vector_index_name for index in indexes):
+            for index in indexes:
+                if index['indexName'] == s3_vector_index_name:
+                    s3_vector_index_arn = index['indexArn']
+                    logger.info(f"s3_vector_index_arn: {s3_vector_index_arn}")
+                    break
+        else:
+            response = client.create_index(
+                vectorBucketArn=s3_vector_bucket_arn,
+                indexName=s3_vector_index_name,
+                dataType='float32',
+                dimension=1024,
+                distanceMetric='cosine'                
+            )
+            logger.info(f"response of create_index: {response}")
+            time.sleep(3)
+            
+            response = client.list_indexes(vectorBucketName=s3_vector_bucket_name)
+            indexes = response.get('indexes', [])
+            for index in indexes:
+                if index['indexName'] == s3_vector_index_name:
+                    s3_vector_index_arn = index['indexArn']
+                    logger.info(f"s3_vector_index_arn: {s3_vector_index_arn}")
+                    break
+            
+        config['s3_vector_index_arn'] = s3_vector_index_arn
+        with open(config_path, "w", encoding="utf-8") as f:
+            json.dump(config, f, indent=2)
+    elif not s3_vector_index_arn:
+        # 인덱스 이름은 있지만 ARN이 없는 경우 ARN을 가져옴
+        response = client.list_indexes(vectorBucketName=s3_vector_bucket_name)
+        indexes = response.get('indexes', [])
+        for index in indexes:
+            if index['indexName'] == s3_vector_index_name:
+                s3_vector_index_arn = index['indexArn']
+                logger.info(f"s3_vector_index_arn: {s3_vector_index_arn}")
+                config['s3_vector_index_arn'] = s3_vector_index_arn
+                with open(config_path, "w", encoding="utf-8") as f:
+                    json.dump(config, f, indent=2)
+                break
+            
+    knowledge_base_id = boto3.client('bedrock-agent').create_knowledge_base(
+        name=f"knowledge-base-for-{projectName}-{bedrock_region}",
+        description=f"Knowledge base for {knowledge_base_name}",
+        roleArn=f"arn:aws:iam::{accountId}:role/{role_name}",
+        knowledgeBaseConfiguration={
+            "type": "VECTOR",
+            "vectorKnowledgeBaseConfiguration": {
+                "embeddingModelArn": f"arn:aws:bedrock:{bedrock_region}:{accountId}:model/amazon.titan-embed-text-v2:0", 
+                "embeddingModelConfiguration": {
+                    "bedrockEmbeddingModelConfiguration": {
+                        "dimensions": 1024,
+                        "embeddingDataType": "FLOAT32"
+                    }
+                }
+            }
+        },
+        storageConfiguration={
+            "type": "S3_VECTORS",
+            "s3VectorsConfiguration": {
+                "vectorBucketArn": s3_vector_bucket_arn,
+                "indexArn": s3_vector_index_arn,
+                "indexName": s3_vector_index_name
+            }
+        }
+    )
+    config['knowledge_base_id'] = knowledge_base_id
+    with open(config_path, "w", encoding="utf-8") as f:
+        json.dump(config, f, indent=2)
 
 def get_contents_type(file_name):
     if file_name.lower().endswith((".jpg", ".jpeg")):
